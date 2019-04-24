f69ffb4a5314bf924027b5e6353f437cd57d745d
watkins-tim <watkins.timothy.m@gmail.com>
2019-04-24 18:20:37 -0400

    debug api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,1 @@
-const api_url=function(){
-  let api_url = process.env.API_URL || 'http://localhost:8080';
-  console.log(process.env.API_URL);
-  return api_url;
-}
-
-export const {API_URL} = api_url();
+exports.API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
